65e936f3fcaa1c5abcf5cd8b4886ab29d2ebb20c
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-05-16 12:20:30 +0500

    Team Quvonchbek

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import images from "./imageData";
 
 function App() {
+  const [Value, setValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="chat-card">
+          <div className="card-top">
+            {images?.map((data) => {
+              return (
+                <img width="100%" src={data.image} alt="avatar" />
+              )
+            })}
+          </div>
+          <div className="comments e">
+            <div className="comment my">I commented on Figma, I want to add some fancy icons. Do you have any icon set?</div>
+          </div>
+          <div className="comments o">
+            <div className="comment you">I am in a process of designing some. When do you need them?</div>
+          </div>
+          <form>
+            <input onChange={(e) => setValue(e.target.value)} type="text" placeholder="Write a message..." />
+            <i onClick={() => { console.log(Value); }} className="fa-solid fa-paper-plane"></i>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/imageData.js
@@ -0,0 +1,37 @@
+import avatar1 from "./image/avatar1.png";
+import avatar2 from "./image/avatar2.png";
+import avatar3 from "./image/avatar3.png";
+import avatar4 from "./image/avatar4.png";
+import avatar5 from "./image/avatar5.png";
+import avatar6 from "./image/avatar6.png";
+import avatar7 from "./image/avatar7.png";
+import avatar8 from "./image/avatar8.png";
+
+const images = [
+    {
+        image: avatar1
+    },
+    {
+        image: avatar2
+    },
+    {
+        image: avatar3
+    },
+    {
+        image: avatar4
+    },
+    {
+        image: avatar5
+    },
+    {
+        image: avatar6
+    },
+    {
+        image: avatar7
+    },
+    {
+        image: avatar8
+    }
+]
+
+export default images;
